5ebb341e115f4118b13961a8d44aeed4ad300f8a
Ivomonopoli <ivomonopoli2001@gmail.com>
2022-10-14 00:06:03 -0300

    commit 3

---
+++ src/Container/detail/itemdetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function itemdetail() {
+  return (
+    true
+  )
+}
+
+export default itemdetail
---
+++ src/Container/detail/itemdetailcontainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function itemdetailcontainer() {
+  return (
+    <div>itemdetailcontainer</div>
+  )
+}
+
+export default itemdetailcontainer
